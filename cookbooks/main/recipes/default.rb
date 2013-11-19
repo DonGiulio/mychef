a3e6e3ba7d410844ba9b60499f7346d847420d3a
Giulio Giraldi <giuliosantegiraldi@printer.local>
2013-11-19 16:10:48 +0100

    removing passwords

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -1,13 +1,1 @@
 package "git-core"
-
-# gem install rake rspec extlib systemu json
-
-node[:user].each do |name, val|
-  # puts val.to_yaml
-  user val[:name] do
-    password val[:password]
-    gid val[:name]
-    home "/home/#{val[:name]}"
-    supports manage_home: true
-  end
-end
