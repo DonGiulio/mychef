d3832ea194a578872ef3bb0bfa1b222d56c299e7
Giulio Giraldi <giuliosantegiraldi@printer.local>
2013-11-19 16:13:38 +0100

    fixing data

--- cookbooks/main/metadata.rb
+++ cookbooks/main/metadata.rb
@@ -1,8 +1,8 @@
-name              'PJForex'
-maintainer        'Giulio Sante Giraldi'
-maintainer_email  'giulio.giraldi@pjforex.com'
+name              'project'
+maintainer        'dongiulio'
+maintainer_email  'g...@xxxxx.com'
 license           'Private'
-description       'Provisions the environment for PJForex'
+description       'Provisions the environment'
 version           '1.0.6'
 
 depends 'nginx'
