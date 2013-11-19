26b6f278be5eeda1513492cbdf549d05ff06363a
Giulio Giraldi <giuliosantegiraldi@printer.local>
2013-11-19 15:36:31 +0100

    first import

---
+++ cookbooks/main/metadata.rb
@@ -0,0 +1,8 @@
+name              'PJForex'
+maintainer        'Giulio Sante Giraldi'
+maintainer_email  'giulio.giraldi@pjforex.com'
+license           'Private'
+description       'Provisions the environment for PJForex'
+version           '1.0.6'
+
+depends 'nginx'
